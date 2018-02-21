1fdb1b2dcec25887722da5a4260621c9a49b0317
Banajit Goswami <bgoswami@codeaurora.org>
2018-02-20 22:55:16 -0800

    msmnile: add audio kernel modules to the product packages
    Add audio modules to product package for msmnile.
    
    Change-Id: I25e0629a696427df5ce5ead88a169c39416b392e

--- msmnile.mk
+++ msmnile.mk
@@ -76,6 +76,30 @@ PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 # Audio configuration file
 -include $(TOPDIR)hardware/qcom/audio/configs/msmnile/msmnile.mk
 
+#Audio DLKM
+AUDIO_DLKM := audio_apr.ko
+AUDIO_DLKM += audio_wglink.ko
+AUDIO_DLKM += audio_q6_pdr.ko
+AUDIO_DLKM += audio_q6_notifier.ko
+AUDIO_DLKM += audio_adsp_loader.ko
+AUDIO_DLKM += audio_q6.ko
+AUDIO_DLKM += audio_usf.ko
+AUDIO_DLKM += audio_pinctrl_wcd.ko
+AUDIO_DLKM += audio_swr.ko
+AUDIO_DLKM += audio_wcd_core.ko
+AUDIO_DLKM += audio_swr_ctrl.ko
+AUDIO_DLKM += audio_wsa881x.ko
+AUDIO_DLKM += audio_platform.ko
+AUDIO_DLKM += audio_hdmi.ko
+AUDIO_DLKM += audio_stub.ko
+AUDIO_DLKM += audio_wcd9xxx.ko
+AUDIO_DLKM += audio_mbhc.ko
+AUDIO_DLKM += audio_wcd9360.ko
+AUDIO_DLKM += audio_wcd_spi.ko
+AUDIO_DLKM += audio_native.ko
+AUDIO_DLKM += audio_machine_msmnile.ko
+PRODUCT_PACKAGES += $(AUDIO_DLKM)
+
 PRODUCT_PACKAGES += fs_config_files
 
 #A/B related packages
