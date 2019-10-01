9c16c2dd663bbb347b900bb1cc3bbac40c1a0259
Ravi Kiran Kasula <rkasul@codeaurora.org>
2019-10-01 14:13:11 +0530

    msmnile R: Compile fixes
    Removes PRODUCT_STATIC_BOOT_CONTROL_HAL
    
    Bug: 134090332
    Change-Id: Id55f4321540bab77c663140ad279064cd2a6500b

--- msmnile.mk
+++ msmnile.mk
@@ -253,12 +253,6 @@ PRODUCT_HOST_PACKAGES += \
 #Boot control HAL test app
 PRODUCT_PACKAGES_DEBUG += bootctl
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-  bootctrl.msmnile \
-  librecovery_updater_msm \
-  libz \
-  libcutils
-
 PRODUCT_PACKAGES += \
   update_engine_sideload
 
