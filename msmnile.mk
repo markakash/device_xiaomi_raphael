aaad1b655abee951f3e5fb900f94377e1cd65890
Sujin Panicker <spanic@codeaurora.org>
2020-07-24 11:37:25 +0530

    conf_files: set higher priority to c2 than OMX
    Change-Id: If047caf630df17c9dfa94b67fab9f6259aa47e25

--- msmnile.mk
+++ msmnile.mk
@@ -113,9 +113,7 @@ ifeq ($(GENERIC_ODM_IMAGE),true)
   PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.ccodec=4
   PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=1000
 else
-  $(warning "Enabling codec2.0 SW only for non-generic odm build variant")
-  #Rank OMX SW codecs lower than OMX HW codecs
-  PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank.sw-audio=1
+  $(warning "Enabling codec2.0 non-audio SW only for non-generic odm build variant")
   PRODUCT_PROPERTY_OVERRIDES += debug.stagefright.omx_default_rank=0
 endif
 
