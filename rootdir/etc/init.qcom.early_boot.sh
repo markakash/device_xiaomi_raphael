c98abab652dcafc481cb7c41ac0d563fecfbe057
Sriharsha Vasudevachar <spvasu@codeaurora.org>
2020-10-01 18:15:31 +0530

    early boot: setting vendor.gralloc.disable_ahardware_buffer
    setting display property vendor.gralloc.disable_ahardware_buffer
    to 1 for 8917/QM215
    
    Change-Id: Ie64e7a0fd9235287beb33a9dfc7acb400d94b0fa

--- rootdir/etc/init.qcom.early_boot.sh
+++ rootdir/etc/init.qcom.early_boot.sh
@@ -262,10 +262,11 @@ case "$target" in
                     log -t BOOT -p i "SDM429 early_boot prop set for: HwID '$soc_hwid'"
                 fi
                 ;;
-            303|307|308|309|320)
+            303|307|308|309|320|386|436)
                 # Vulkan is not supported for 8917 variants
                 setprop vendor.opengles.version 196608
                 setprop persist.graphics.vulkan.disable true
+                setprop vendor.gralloc.disable_ahardware_buffer 1
                 ;;
             *)
                 setprop vendor.opengles.version 196608
