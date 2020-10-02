73d7e7ce680c20dd7f76a3317bab333707f47017
Roopesh Nataraja <roopeshr@codeaurora.org>
2020-10-01 17:53:21 -0700

    common: Set TARGET_KERNEL_VERSION only if it's not set
    TARGET_KERNEL_VERSION and TARGET_USES_NEW_ION can be
    set in product mk file. Set these flags in base.mk only
    if it's not set already.
    
    Change-Id: Idc23a43eb81688b012e2dcd2aa506e68eaa07aff

--- base.mk
+++ base.mk
@@ -1,5 +1,5 @@
 # define flag to determine the kernel
-TARGET_KERNEL_VERSION := $(shell ls kernel | grep "msm-*" | sed 's/msm-//')
+TARGET_KERNEL_VERSION ?= $(shell ls kernel | grep "msm-*" | sed 's/msm-//')
 
 # Set TARGET_USES_NEW_ION for 4.14 and higher kernels
 ifeq ($(TARGET_KERNEL_VERSION),$(filter $(TARGET_KERNEL_VERSION),3.18 4.4 4.9))
