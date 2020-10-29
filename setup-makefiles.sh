544341cdff6585410c3896128f0d3ad993aa864c
Adhitya Mohan <me@adhityamohan.in>
2020-10-29 09:25:47 +0530

    raphael: Update Radio, IMS, QMI and DPM from LA.UM.9.8.r1-02700-SDM710.0

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -21,7 +21,7 @@ set -e
 DEVICE=raphael
 VENDOR=xiaomi
 
-INITIAL_COPYRIGHT_YEAR=2019
+INITIAL_COPYRIGHT_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -29,7 +29,7 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 ROOT="$MY_DIR"/../../..
 
-HELPER="$ROOT"/vendor/aosp/build/tools/extract_utils.sh
+HELPER="$ROOT"/vendor/yaap/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
