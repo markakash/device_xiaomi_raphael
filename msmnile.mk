621a533ff82d48e6b270bb4e7d2797d5311a4c63
Akash Srivastava <akashniki@gmail.com>
2020-11-04 21:38:18 +0100

    raphael: Rename to Project 404

--- msmnile.mk
+++ msmnile.mk
@@ -39,7 +39,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-p404
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
