d653ba2ba757ca315ce1a89710805973086b1982
Akash Srivastava <akashniki@gmail.com>
2020-11-05 14:52:13 +0100

    raphael: Build with gapps

--- p404_raphael.mk
+++ p404_raphael.mk
@@ -21,6 +21,9 @@ $(call inherit-product, vendor/404/configs/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := p404_raphael
 PRODUCT_DEVICE := raphael
