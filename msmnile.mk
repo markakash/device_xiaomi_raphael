f3d988e1f6d7df4341a6d4d40b04216052e0eb5d
Akash Srivastava <akashniki@gmail.com>
2021-01-26 15:35:53 +0000

    raphael: Switch to OSS camera stack

--- msmnile.mk
+++ msmnile.mk
@@ -131,7 +131,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
